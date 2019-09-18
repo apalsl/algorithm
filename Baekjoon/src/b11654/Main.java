ccb59778573ee0f28f892db1ecb948f2f9360969
HRLEE <apalsl123@gmail.com>
2019-09-18 22:30:47 +0900

    아스키코드 변환

---
+++ Baekjoon/src/b11654/Main.java
@@ -0,0 +1,15 @@
+package b11654;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		String s = sc.next();
+		int v = s.trim().charAt(0);
+		System.out.println(v);
+	}
+}
